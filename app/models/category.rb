5474091632f8ac9feae16d5acf0f802a9f5e89c5
Rita <rita.lu@yahoo.com.tw>
2018-01-28 23:58:09 +0800

    link category and restaurant model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  # 建立Model資料關聯
+  has_many :restaurants, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates_presence_of :name
+
+# 建立Model資料關聯
+  belongs_to :category, optional: true
 end
