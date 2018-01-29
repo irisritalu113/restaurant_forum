6f51fcf56dc2e2ce3206a040e17ad68af3da2a75
Rita <rita.lu@yahoo.com.tw>
2018-01-29 21:38:53 +0800

    add form on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,6 +4,27 @@ class Admin::CategoriesController < ApplicationController
   
   def index
     @categories = Category.all
+    @category = Category.new
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      #如果儲存成功，讓使用者繼續瀏覽資料
+      flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+    
+    else
+      # 重新 render index 樣板，需要再額外傳入 index 需要的 @categories 實例變數
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+  # 使用 Strong parameter 來允許表單傳入資料
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
