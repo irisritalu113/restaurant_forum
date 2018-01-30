83ee7ac21900b341c46743dcaf65aca1126f8cdf
Rita <rita.lu@yahoo.com.tw>
2018-01-30 23:24:21 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show] do
-      resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    
+    # 瀏覽所有餐廳的最新動態
+    collection do
+      get :feeds
+    end
+
+    # 瀏覽個別餐廳的 Dashboard
+    member do
+      get :dashboard
+    end
   end
 
   resources :users, only: [:show, :edit, :update]
@@ -15,4 +25,5 @@ Rails.application.routes.draw do
     resources :categories
     root "restaurants#index"
   end
+
 end
