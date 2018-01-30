76c87002ebe0cf2035107d360336db740a4164a7
Rita <rita.lu@yahoo.com.tw>
2018-01-30 22:44:20 +0800

    user can view commented restaurants on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
-  def show  
+  def show 
+    @commented_restaurants = @user.restaurants.uniq
+    @user = User.find(params[:id])
+    @commented_restaurants = @user.restaurants 
   end
 
   def edit
