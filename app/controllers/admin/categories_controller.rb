0e3b0999f36d0b58a23a6962e4706cdfe8a1d78e
Rita <rita.lu@yahoo.com.tw>
2018-01-30 08:14:47 +0800

    rebuit controller lesson, miss s1

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,12 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :authenticate_admin
-  before_action :set_category
+  
+  
   
   def index
     @categories = Category.all
 
     if params[:id]
-      set_category
+      @category = Category.find(params[:id])
     else
       @category = Category.new
     end
@@ -27,7 +27,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def update
-    set_category
+    @category = Category.find(params[:id])
     if @category.update(category_params)
       redirect_to admin_categories_path
       flash[:notice] = "category was successfully updated"
@@ -38,7 +38,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def destroy
-    set_category
+    @category = Category.find(params[:id])
     @category.destroy
     flash[:alert] = "category was successfully deleted"
     redirect_to admin_categories_path
