5167545d3c96297a5c0f197a7f3f3762c5392867
Rita <rita.lu@yahoo.com.tw>
2018-01-30 22:10:06 +0800

    user can upload avatar先在rails generate uploader avatar，再去user.rb加入上載器

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
+
   has_many :comments
 
-
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
   end
