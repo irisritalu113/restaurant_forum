31a870655ec1e290670c20b4c4379b8030c8e0cd
Rita <rita.lu@yahoo.com.tw>
2018-01-30 23:52:07 +0800

    add dependency between models更新關聯設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   # 建立Model資料關聯
   validates_presence_of :name
   # dependent: :destroy，表示在刪除某筆 Category 資料時，需要一併刪除所有關聯的 Restaurant 資料
-  has_many :restaurants, dependent: :destroy
+  # 如果分類下已有餐廳，就不允許刪除分類（刪除時拋出 Error)
+  has_many :restaurants, dependent: :restrict_with_error
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,6 +6,7 @@ class Restaurant < ApplicationRecord
   belongs_to :category, optional: true
   delegate :name, to: :category, prefix: true, allow_nil: true
 
-  has_many :comments
+  # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
+  has_many :comments, dependent: :destroy
 
 end
