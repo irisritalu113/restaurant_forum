4ca200c5b17f03abd1ec4b5b749c27116fe300b6
Rita <rita.lu@yahoo.com.tw>
2018-01-30 20:21:25 +0800

    user can read comments on restaurant show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
   
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @comment = @restaurant.comments.build(comment_params)
-    @comment.user = current_user
-    @comment.save!
-    redirect_to restaurant_path(@restaurant)
+    @restaurant = Restaurant.find(params[:restaurant_id]) #確認評論屬於哪一個@restaurant
+    @comment = @restaurant.comments.build(comment_params) #建立屬於@restaurant的評論，評論內容來自表單傳入的資料
+    @comment.user = current_user                          #關聯外鍵user.id
+    @comment.save!                                        #將評論存入資料庫，與.save不同處在於不論驗證或錯誤都會送資料
+    redirect_to restaurant_path(@restaurant)              #回到restaurants/show
   end
 
   private
 
+  #要透過permit將允許的表單內容登記下來
   def comment_params
     params.require(:comment).permit(:content)
   end
