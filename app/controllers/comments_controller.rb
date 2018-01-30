8bf704c0ff3c81583c9ed1ee82f2535942c746cc
Rita <rita.lu@yahoo.com.tw>
2018-01-30 22:29:24 +0800

    add user name to seed data and fake data

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,10 +12,10 @@ class CommentsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = Comment.find(params[:id])
 
-    # if current_user.admin?
-    #   @comment.destroy
-    #   redirect_to restaurant_path(@restaurant)
-    # end
+    if current_user.admin?
+      @comment.destroy
+      redirect_to restaurant_path(@restaurant)
+    end
   end
   
   private
