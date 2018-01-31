5b5851ff50d78b6a5612caab6182c270c22b447b
Rita <rita.lu@yahoo.com.tw>
2018-01-31 21:59:22 +0800

    count favorites size in favorite/unfavorite action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,6 +25,7 @@ class RestaurantsController < ApplicationController
   def favorite
     @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
@@ -34,6 +35,7 @@ class RestaurantsController < ApplicationController
     # 由於 favorites 只在 Action 內部使用，所以單純用區域變數 (local variable) 的命名方法，而沒有特別宣告成實例變數 (instance variable)。這是屬於撰寫程式碼的 style，在這裡，即使寫成實例變數，也不會影響功能的執行。
     favorites = Favorite.where(restaurant: @restaurant, user: current_user)
     favorites.destroy_all
+    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 
